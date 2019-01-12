373b2971a07c46cc28a7f9d6fcd81beb8d467975
MoonCheung <salvador23@163.com>
2019-01-12 19:41:54 +0800

    update detail组件

---
+++ src/components/BookInfo.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="bookinfo">
+    <div class="thumb">
+      <img class="back"
+           :src="info.image"
+           mode="aspectFilt">
+      <img class="img"
+           :src="info.image"
+           mode="aspectFit">
+      <div class="info">
+        <div class="title">
+          {{info.title}}
+        </div>
+        <div class="author">
+          {{info.author}}
+        </div>
+      </div>
+    </div>
+    <div class="detail">
+      <img :src="userinfo.image" class="avatar" mode="aspectFit">
+      {{userinfo.name}}
+      <div class="right text-primary">
+        {{info.rate}}分
+        <Rate :value="info.rate"></Rate>
+      </div>
+    </div>
+    <div class="detail">
+      {{info.publisher}}
+      <div class="right">
+        {{info.price}}
+      </div>
+    </div>
+    <div class="tags">
+      <div class="badge" v-for="tag in info.tags" :key="tag">{{tag}}</div>
+    </div>
+    <div class="summary">
+      <p class="" v-for="(sum, i) in info.summary" :key="i">{{sum}}</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Rate from "@/components/Rate"
+
+export default {
+  props: ['info'],
+  components: {
+    Rate
+  },
+  // 计算属性将被混入到 Vue 实例中
+  computed: {
+    userinfo(){
+      return this.info.user_info || {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.bookinfo {
+  font-size: 14px;
+  .right{
+    float: right;
+  }
+  .badge{
+    display: inline-block;
+    margin: 5px;
+    padding: 5px;
+    border-radius: 10px;
+    border: 1px #EA5A49 solid;
+    color: #EA5A49
+  }
+  .summary{
+    padding: 0 15px;
+    margin-top: 10px;
+    p{
+      text-indent: 2em;
+      font-size: 14px;
+    }
+  }
+  .detail{
+    padding: 5px 10px;
+    .avatar{
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      vertical-align: middle;
+    }
+  }
+  .thumb {
+    width: 750rpx;
+    height: 500rpx;
+    overflow: hidden;
+    position: relative;
+    .back {
+      filter: blur(15px);
+      width: 100%;
+    }
+    .img {
+    position: absolute;
+    width: 100%;
+    height: 300rpx;
+    left: 0;
+    top: 30rpx;
+    }
+    .info {
+      color: white;
+      position: absolute;
+      width: 100%;
+      left: 0;
+      top: 330rpx;
+      text-align: center;
+      .title{
+        font-size: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,19 +1,88 @@
 <template>
-  <div>图书:{{bookid}}</div>
+  <div>
+    <BookInfo :info="info" ></BookInfo>
+    <div class="comment">
+      <textarea v-model="comment"
+                class="textarea"
+                :maxlength="100"
+                placeholder="请输入图书短评"
+                ></textarea>
+      <div class="location">
+        地理位置:
+        <switch color="#EA5A49" :checked="location" @change="getGeo" ></switch>
+        <span class="text-primary">{{ location }}</span>
+      </div>
+      <div class="phone">
+        手机型号:
+        <switch color="#EA5A49" :checked="phone" @change="getPhone" ></switch>
+        <span class="text-primary">{{ phone }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { get } from '@/utils/index'
+import BookInfo from '@/components/BookInfo'
 
 export default {
+  components: {
+    BookInfo
+  },
   data () {
     return {
-      bookid: ''
+      bookid: '',
+      info: {},
+      comment: '',
+      location: '',
+      phone: ''
     }
   },
   methods: {
     async getDtail(){
       const info = await get('/api/bookdetail', {id: this.bookid})
+      wx.setNavigationBarTitle({
+        title: info.title
+      })
+      this.info = info
+    },
+    getGeo(e){
+      const ak = "KIeNfIUwXbDSlLTTsEy187HeBrNuQ7fI"
+      let url = "http://api.map.baidu.com/geocoder/v2/"
+      if(e.target.value){
+        wx.getLocation({
+          success: geo =>{
+            wx.request({
+              url,
+              data: {
+                ak,
+                location: `${geo.latitude},${geo.longitude}`,
+                output: 'json'
+              },
+              success: res =>{
+                if(res.data.status == 0){
+                  this.location = res.data.result.addressComponent.city
+                }else{
+                  this.location = '未知地点'
+                }
+              }
+            })
+          }
+        })
+      }else{
+        //没选位置
+        this.location = ''
+      }
+    },
+    getPhone(e){
+      console.log('手机型号获取:')
+      if(e.target.value){
+        const getPhone = wx.getSystemInfoSync();
+        this.phone = getPhone.model
+      }else{
+        //没选手机
+        this.phone = "";
+      }
     }
   },
   mounted () {
@@ -24,5 +93,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.comment{
+  margin-top: 10px;
+  font-size: 14px;
+  .textarea{
+    background: #eee;
+    padding: 10rpx;
+    width: 730rpx;
+    height: 200rpx;
+  }
+  .location{
+    margin-top:5px;
+    padding:5px 10px;
+  }
+  .phone{
+    margin-top:5px;
+    padding:5px 10px;
+  }
+}
 </style>
